9661ec5b0fb97e41a150ee93ed85363a349be6c8
Abdelrahman-Shahda <abdelrahmanshahda@yahoo.com>
2021-04-26 23:16:02 +0200

    Adjust APIDOCS

--- doc/api_project.js
+++ doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-04-26T13:22:32.533Z",
+    "time": "2021-04-26T20:02:07.865Z",
     "url": "https://apidocjs.com",
     "version": "0.27.1"
   }
