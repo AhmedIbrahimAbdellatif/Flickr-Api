048bb0626e4180b8f5a6afde2733ac9fd9708a27
Abdelrahman-Shahda <abdelrahmanshahda@yahoo.com>
2021-04-28 17:58:44 +0200

    Add validation-request middleware

---
+++ src/error/request-validation.js
@@ -0,0 +1,19 @@
+const { CustomError } = require('./custom-error')
+
+class RequestValidationError extends CustomError {
+    statusCode = 400;
+    constructor(errors){
+        super('Request Validation Error')
+        this.errors = errors
+        Object.setPrototypeOf(this, RequestValidationError.prototype);
+
+    }
+    serializeError() {
+        return {
+          message: this.errors[0].msg,
+        };
+      }
+}
+module.exports = {
+    RequestValidationError
+}
