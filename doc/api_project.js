7a8ecdc85cadad98d86397a07474389388d16fad
Abdelrahman-Shahda <abdelrahmanshahda@yahoo.com>
2021-06-01 13:53:08 +0200

    Update documentations

--- doc/api_project.js
+++ doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-01T09:32:07.093Z",
+    "time": "2021-06-01T11:52:01.760Z",
     "url": "https://apidocjs.com",
     "version": "0.27.1"
   }
